979111b81b12447999a7ea04c3b4ebffd0fcff3e
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-09 02:28:49 -0300

    criação/controller: foi criado controller para atualizar um usuário.

--- src/routes/userRoutes/userRoutes.ts
+++ src/routes/userRoutes/userRoutes.ts
@@ -1,19 +1,13 @@
 import express from 'express';
 import { UserCreateControler } from '../../controllers/ControllersUser';
+import { UpdateUserController } from '../../controllers/ControllersUser/ControllersUpdatedUser';
 
 const createUser = new UserCreateControler(); //criação de um usuário
+const updatedUser = new UpdateUserController(); // atualização de um usuário
 
 const Router = express.Router();
 
 Router.post('/users', createUser.handle);
-
-Router.get('/index', (request, response) => {
-  return response.json({
-    users: [
-      { id: 1, nome: 'Luann' },
-      { id: 2, nome: 'Pedro' },
-    ],
-  });
-});
+Router.put('/users/:id', updatedUser.handle);
 
 export { Router };
