fafc384b3976fc186d57b6500e07179fd56188cf
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-09 03:00:36 -0300

    criação/services: foi criado services para esta buscando por um usuário.

--- src/routes/userRoutes/userRoutes.ts
+++ src/routes/userRoutes/userRoutes.ts
@@ -1,16 +1,20 @@
 import express from 'express';
+
 import { UserCreateControler } from '../../controllers/ControllersUser';
 import { UpdateUserController } from '../../controllers/ControllersUser/ControllersUpdatedUser';
 import { DeletedUserController } from '../../controllers/ControllersUser/ControllersDeletedUser';
+import { GetaAllUserController } from '../../controllers/ControllersUser/ControllersGetAllUser';
 
 const createUser = new UserCreateControler(); //criação de um usuário.
 const updatedUser = new UpdateUserController(); // atualização de um usuário.
 const deletedUser = new DeletedUserController(); // excluido um usuário.
+const getlAllUser = new GetaAllUserController(); // buscar todos usuários.
 
 const Router = express.Router();
 
 Router.post('/users', createUser.handle);
 Router.put('/users/:id', updatedUser.handle);
 Router.delete('/users/:id', deletedUser.handle);
+Router.get('/users', getlAllUser.handle);
 
 export { Router };
