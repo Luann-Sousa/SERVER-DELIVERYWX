1d188da3b79bd5aecaa48065ed2015483c11e205
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-09 02:35:25 -0300

    finalizado/services: foi finalizado services de deletar um usuário.

---
+++ src/services/ServicesUser/ServiceDeletedUser.ts
@@ -0,0 +1,19 @@
+import { getCustomRepository } from 'typeorm';
+import { UserRepositorie } from '../../repositories/RepositoriesUser';
+
+class DeletedUserService {
+  async execute(id: string) {
+    const userRepositry = getCustomRepository(UserRepositorie);
+
+    const userExits = await userRepositry.findOne(id);
+    console.log(userExits);
+    if (!userExits) {
+      throw new Error(
+        'Não possui esse usuário em nosso sistema tente mais tarde!',
+      );
+    }
+    return await userRepositry.delete(id);
+  }
+}
+
+export { DeletedUserService };
