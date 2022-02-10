9ac0c39656b918a2c9079cd452c9d293adc32425
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-10 04:29:53 -0300

    criação/routes: foi criada a rota de buscar por endereços relacionado a um usuário especifico.

--- src/controllers/ControllersAddres/ControllersGetAllAddressUsers.ts
+++ src/controllers/ControllersAddres/ControllersGetAllAddressUsers.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { GetAlllAddressService } from '../../services/ServicesAddress/ServiceGetAllAddress';
 
-class GetAlllAddressController {
+class GetAlllAddressUsersController {
   async handle(request: Request, response: Response) {
     const service = new GetAlllAddressService();
 
@@ -14,4 +14,4 @@ class GetAlllAddressController {
   }
 }
 
-export { GetAlllAddressController };
+export { GetAlllAddressUsersController };
--- src/routes/addressRoutes/addressRoutes.ts
+++ src/routes/addressRoutes/addressRoutes.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import { CreateAddressController } from '../../controllers/ControllersAddres/ControllersCreateAddress';
 import { GetAlllAddressController } from '../../controllers/ControllersAddres/ControllersGetAllAddress';
+import { GetAlllAddressUsersController } from '../../controllers/ControllersAddres/ControllersGetAllAddressUsers';
 
 const RouterAddres = express.Router();
 
 const createAddres = new CreateAddressController(); //criação de um endereço.
-const getAllAddress = new GetAlllAddressController(); // busca por todos endereços.
+const getAllAddress = new GetAlllAddressController(); // buscar por todos endereços.
+const getAllAddresUsers = new GetAlllAddressUsersController(); //buscar por endereços relacionado a um usuário especifico.
 
 RouterAddres.post('/address', createAddres.handle);
 RouterAddres.get('/address', getAllAddress.handle);
+RouterAddres.get('/address', getAllAddresUsers.handle);
 
 export { RouterAddres };
