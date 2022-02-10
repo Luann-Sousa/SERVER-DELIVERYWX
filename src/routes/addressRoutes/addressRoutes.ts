502c04ec899c1b615df82ccf64b8f7a28b2a613c
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-10 05:13:47 -0300

    criação/routes: foi criada a rota de atualizar endereços relacionado a um usuário especifico.

--- src/routes/addressRoutes/addressRoutes.ts
+++ src/routes/addressRoutes/addressRoutes.ts
@@ -1,16 +1,20 @@
 import express from 'express';
+
 import { CreateAddressController } from '../../controllers/ControllersAddres/ControllersCreateAddress';
 import { GetAlllAddressController } from '../../controllers/ControllersAddres/ControllersGetAllAddress';
 import { GetAlllAddressUsersController } from '../../controllers/ControllersAddres/ControllersGetAllAddressUsers';
+import { UpdateUserController } from '../../controllers/ControllersAddres/ControllersUpdatedAddres';
 
 const RouterAddres = express.Router();
 
 const createAddres = new CreateAddressController(); //criação de um endereço.
-const getAllAddress = new GetAlllAddressController(); // buscar por todos endereços.
+const getAllAddress = new GetAlllAddressController(); //buscar por todos endereços.
 const getAllAddresUsers = new GetAlllAddressUsersController(); //buscar por endereços relacionado a um usuário especifico.
+const updatedAddresUsers = new UpdateUserController(); //atualizar endereço.
 
 RouterAddres.post('/address', createAddres.handle);
 RouterAddres.get('/address', getAllAddress.handle);
 RouterAddres.get('/address/user', getAllAddresUsers.handle);
+RouterAddres.put('/address/:id', updatedAddresUsers.handle);
 
 export { RouterAddres };
