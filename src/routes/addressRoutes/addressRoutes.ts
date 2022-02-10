e4b6c414c1a83d55a92c1d00adbfb029a39bdb74
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-10 04:20:12 -0300

    criação/routes: foi criado rotas para buscar por todos endereços cadastrados.

--- src/routes/addressRoutes/addressRoutes.ts
+++ src/routes/addressRoutes/addressRoutes.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { CreateAddressController } from '../../controllers/ControllersAddres/ControllersCreateAddress';
+import { GetAlllAddressController } from '../../controllers/ControllersAddres/ControllersGetAllAddress';
 
 const RouterAddres = express.Router();
 
 const createAddres = new CreateAddressController(); //criação de um endereço.
+const getAllAddress = new GetAlllAddressController(); // busca por todos endereços.
 
 RouterAddres.post('/address', createAddres.handle);
+RouterAddres.get('/address', getAllAddress.handle);
 
 export { RouterAddres };
