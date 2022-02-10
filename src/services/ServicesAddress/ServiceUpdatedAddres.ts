2f678b41e36231c83e273a7e19811afd12c899e8
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-10 04:57:23 -0300

    finalizado/servies: foi fializado services para atualizar o endereço de um usuario.

---
+++ src/services/ServicesAddress/ServiceUpdatedAddres.ts
@@ -0,0 +1,56 @@
+import { getCustomRepository } from 'typeorm';
+import { RepositorieAddress } from '../../repositories/RepositoriesAddress';
+
+interface IAddressUpdateProps {
+  id: string;
+  uf: string;
+  city: string;
+  zip_code: string;
+  district: string;
+  street: string;
+  number: number;
+  complement: string;
+  lat: number;
+  long: number;
+  user_id: string;
+}
+
+class ServiceUpdatedAddres {
+  async execute({
+    id,
+    uf,
+    city,
+    zip_code,
+    district,
+    street,
+    number,
+    complement,
+    lat,
+    long,
+    user_id,
+  }: IAddressUpdateProps) {
+    const userRepository = getCustomRepository(RepositorieAddress);
+
+    const user = await userRepository.findOne(id);
+
+    if (!user) {
+      return new Error('Não possui esse usário em nosso sitema tente outro !');
+    }
+
+    user.uf = uf ? uf : user.uf;
+    user.city = city ? city : user.city;
+    user.zip_code = zip_code ? zip_code : zip_code;
+    user.district = district ? district : district;
+    user.street = street ? street : street;
+    user.number = number ? number : number;
+    user.complement = complement ? complement : complement;
+    user.lat = lat ? lat : lat;
+    user.long = long ? long : long;
+    user_id = user_id ? user_id : user_id;
+    const result = await userRepository.save(user);
+
+    return { result };
+  }
+}
+
+export { ServiceUpdatedAddres };
