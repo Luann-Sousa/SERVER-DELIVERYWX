ecc00daf104d52b96ab459e02f374e5ef5b0afce
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-10 03:12:32 -0300

    criação/migrations: foi criando a migração para criação de endereços de usuários.

---
+++ src/database/migrations/1644472554222-Address.ts
@@ -0,0 +1,82 @@
+import { type } from 'os';
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+export class Address1644472554222 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    return queryRunner.createTable(
+      new Table({
+        name: 'addres',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isUnique: true,
+          },
+          {
+            name: 'uf',
+            type: 'varchar',
+          },
+          {
+            name: 'city',
+            type: 'varchar',
+          },
+          {
+            name: 'zip_code',
+            type: 'numeric',
+          },
+          {
+            name: 'district',
+            type: 'varchar',
+          },
+          {
+            name: 'street',
+            type: 'varchar',
+          },
+          {
+            name: 'number',
+            type: 'varchar',
+          },
+          {
+            name: 'complement',
+            type: 'varchar',
+          },
+          {
+            name: 'lat',
+            type: 'numeric',
+          },
+          {
+            name: 'long',
+            type: 'numeric',
+          },
+          {
+            name: 'user_id',
+            type: 'uuid',
+          },
+          {
+            name: 'created_at',
+            type: 'timestap',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestap',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'fk_address_users',
+            columnNames: ['user_id'],
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
