5bf979f3ffa085a461b2f3dbb714347efd20e877
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-11 02:06:38 -0300

    criação/rotas: foi criado rota para listas todos os cartões cadastro.

--- src/routes/cardRoutes/cardRoutes.ts
+++ src/routes/cardRoutes/cardRoutes.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { CardCreateControler } from '../../controllers/ControllersCard/ControllersCreateCard';
+import { GetAlllCardController } from '../../controllers/ControllersCard/ControllersGetAllCard';
 
 const RouterCard = express.Router();
 
 const createCardUsers = new CardCreateControler(); //cirção de um cartão.
+const getAllCardUsers = new GetAlllCardController(); // buscar cartões cadastrados.
 
 RouterCard.post('/v1/cards', createCardUsers.handle);
+RouterCard.get('/v1/cards', getAllCardUsers.handle);
 
 export { RouterCard };
