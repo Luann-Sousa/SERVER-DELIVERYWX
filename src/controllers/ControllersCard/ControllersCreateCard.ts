5f2bce30f48020e7f3f783e1860c52e1efb0e825
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-11 00:52:02 -0300

    finalização/controllers: foi finalizado controllers para criação de card.

---
+++ src/controllers/ControllersCard/ControllersCreateCard.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express';
+import { CreateUserService } from '../../services/ServicesCard/ServiceCreateCard';
+
+class UserCreateControler {
+  async handle(request: Request, response: Response) {
+    const { name_card, number_card, cpf_card, date_card, cvv_card, user_id } =
+      request.body;
+
+    const createUser = new CreateUserService();
+
+    const result = await createUser.execute({
+      name_card,
+      number_card,
+      cpf_card,
+      date_card,
+      cvv_card,
+      user_id,
+    });
+    return response.status(201).json({
+      status: 200,
+      cards: result,
+    });
+  }
+}
+
+export { UserCreateControler };
