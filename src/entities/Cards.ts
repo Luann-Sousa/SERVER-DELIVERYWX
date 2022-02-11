f8f110f90c5291aa403bf3a61bcb320c7585b7d2
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-11 00:26:07 -0300

    finalizado:entities: finalizado a entities de Cards para criar cartão de cretido do usuário

---
+++ src/entities/Cards.ts
@@ -0,0 +1,57 @@
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+} from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+import { User } from './User';
+
+Entity('cards');
+
+class Cards {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name_card: string;
+
+  @Column()
+  number_card: number;
+
+  @Column()
+  cpf_card: string;
+
+  @Column()
+  date_card: Date;
+
+  @Column()
+  cvv_card: number;
+
+  @Column()
+  user_id: string;
+
+  @ManyToOne(() => User, {
+    cascade: true,
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({ name: 'user_id' }) ///qual coluna dentro da minha tabela de videos que tou referenciando
+  user: User;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+export { Cards };
