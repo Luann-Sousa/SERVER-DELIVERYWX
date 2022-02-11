8d8d20f480c39c06574e8a5678456027967d0d77
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-10 22:42:17 -0300

    finalizado/controllers: foi finalizado o controllers de excluir um endereço de um usuário.

--- src/controllers/ControllersAddres/ControllersDeletedAddres.ts
+++ src/controllers/ControllersAddres/ControllersDeletedAddres.ts
@@ -1,2 +1,18 @@
 import { Request, Response } from 'express';
 import { ServiceDeletedAddres } from '../../services/ServicesAddress/ServiceDeletedAddress';
+
+class ControllersDeletedAddres {
+  async handle(request: Request, response: Response) {
+    const { id_addres } = request.params;
+
+    const userDeleted = new ServiceDeletedAddres();
+
+    const user = userDeleted.execute({ id_addres });
+
+    return response.status(200).json({
+      status: 200,
+      user,
+    });
+  }
+}
+export { ControllersDeletedAddres };
