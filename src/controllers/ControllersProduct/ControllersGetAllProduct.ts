71dfb67e1828d681bfe8331a2df3a28a66829e0f
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-12 00:02:48 -0300

    criado/controllers: foi criado  controllers  para fazer busca de usuários.

---
+++ src/controllers/ControllersProduct/ControllersGetAllProduct.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { ServiceGetAlllCardService } from '../../services/ServicesProducts/ServicesGetAllProduct';
+
+class ControllersGetAllCardUsers {
+  async handle(request: Request, response: Response) {
+    const controllersGetAll = new ServiceGetAlllCardService();
+
+    const result = await controllersGetAll.execute();
+
+    return response.status(200).json({
+      status: 200,
+      address: result,
+    });
+  }
+}
+
+export { ControllersGetAllCardUsers };
