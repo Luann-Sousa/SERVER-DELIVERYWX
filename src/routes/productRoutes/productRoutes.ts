6a4930e5e054475ae169bf46267470314017727b
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-12 00:57:20 -0300

    criação/rota: foi criada a rota para atualizar um produto cadastrado.

--- src/routes/productRoutes/productRoutes.ts
+++ src/routes/productRoutes/productRoutes.ts
@@ -2,15 +2,18 @@ import express from 'express';
 import { ControllersCreateProduct } from '../../controllers/ControllersProduct/ControllersCreateProduct';
 import { ControllersGetAllProduct } from '../../controllers/ControllersProduct/ControllersGetAllProduct';
 import { ControllersGetOneUserProduct } from '../../controllers/ControllersProduct/ControllersGetOneUserProduct';
+import { ControllerUpdatedProduct } from '../../controllers/ControllersProduct/ControllerUpdatedProduct';
 
 const createProduct = new ControllersCreateProduct(); //criação de um produto.
 const getAllProduct = new ControllersGetAllProduct(); // buscar todos produtos cadastrados
 const getOneUserProduct = new ControllersGetOneUserProduct(); //buscar todos produtos relacionado a Admin.
+const updatedProduct = new ControllerUpdatedProduct(); //atualizar um produto cadastrado.
 
 const RouterProduct = express.Router();
 
-RouterProduct.post('/products', createProduct.handle);
-RouterProduct.get('/products', getAllProduct.handle);
-RouterProduct.get('/products/one', getOneUserProduct.handle);
+RouterProduct.post('/v1/products', createProduct.handle);
+RouterProduct.get('/v1/products', getAllProduct.handle);
+RouterProduct.get('/v1/products/one', getOneUserProduct.handle);
+RouterProduct.put('/v1/products/updated/:id', updatedProduct.handle);
 
 export { RouterProduct };
