a3d8fc8f1861035eebe5055e26724ca23687570d
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-12 04:43:35 -0300

    criação/services: foi criado service para buscar pagaemntos relacionado a usuarios e cards especificos.

---
+++ src/services/ServicesPayment/ServiceGetAllPayment.ts
@@ -0,0 +1,18 @@
+import { getRepository } from 'typeorm';
+import { Payments } from '../../entities/Payment';
+
+class ServiceGetAllPayment {
+  async execute() {
+    const productRepositorie = getRepository(Payments);
+
+    const products = await productRepositorie.find({
+      relations: ['user', 'card'],
+    });
+
+    console.log(products);
+
+    return products;
+  }
+}
+
+export { ServiceGetAllPayment };
