567055ab1111e9da25982251a10b17858c594466
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-12 00:13:24 -0300

    finalizado/controllers: foi finalizado controllers para buscar produtos cadratos de cada do de estabelicimento.

---
+++ src/controllers/ControllersProduct/ControllersGetOneUserProduct.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { ServiceGetOneUserProduct } from '../../services/ServicesProducts/ServiceGetOneUserProduct';
+
+class ControllersGetOneUserProduct {
+  async handle(request: Request, response: Response) {
+    const controllersProduct = new ServiceGetOneUserProduct();
+
+    const result = await controllersProduct.execute();
+
+    return response.status(200).json({
+      status: 200,
+      address: result,
+    });
+  }
+}
+
+export { ControllersGetOneUserProduct };
