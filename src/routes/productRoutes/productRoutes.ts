74a02f95fc0b529c7b61f3387bcda09b45458f95
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-12 00:06:34 -0300

    criaçao/rotas: foi criada a rota para buscar por todos produtos.

--- src/routes/productRoutes/productRoutes.ts
+++ src/routes/productRoutes/productRoutes.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { ControllersCreateProduct } from '../../controllers/ControllersProduct/ControllersCreateProduct';
+import { ControllersGetAllProduct } from '../../controllers/ControllersProduct/ControllersGetAllProduct';
 
 const createProduct = new ControllersCreateProduct(); //criação de um produto.
+const getAllProduct = new ControllersGetAllProduct(); // buscar todos produtos cadastrados
 
 const RouterProduct = express.Router();
 
 RouterProduct.post('/products', createProduct.handle);
+RouterProduct.get('/products', getAllProduct.handle);
 
 export { RouterProduct };
