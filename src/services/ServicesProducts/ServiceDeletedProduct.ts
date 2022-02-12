43a5d225d9b60fca2874d2834ba30d2086a808f3
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-12 01:06:19 -0300

    finalizado/services: foi criado finalizado  para exluir um produto cadastrado por um usuário.

---
+++ src/services/ServicesProducts/ServiceDeletedProduct.ts
@@ -0,0 +1,23 @@
+import { getCustomRepository } from 'typeorm';
+import { RepositoriesProduct } from '../../repositories/RepositorieProduct';
+
+interface IProductProps {
+  id_product: string;
+}
+
+class ServiceDeletedAddres {
+  async execute({ id_product }: IProductProps) {
+    const repositorieDeleted = getCustomRepository(RepositoriesProduct);
+
+    const productAllExists = await repositorieDeleted.findOne(id_product);
+
+    if (!productAllExists) {
+      return new Error('Não existe esse produto cadastrado tente outro !');
+    }
+    const productDeleted = await repositorieDeleted.delete(id_product);
+
+    return productDeleted;
+  }
+}
+
+export { ServiceDeletedAddres };
