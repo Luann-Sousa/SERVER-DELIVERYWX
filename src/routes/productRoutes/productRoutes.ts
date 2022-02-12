586ac6838cb4cc2449779c1aa34c1bd6d7e42e24
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-12 00:17:24 -0300

    criação/rota: foi criada a rota para buscar produtos cadastrados feito pelo Admin.

--- src/routes/productRoutes/productRoutes.ts
+++ src/routes/productRoutes/productRoutes.ts
@@ -1,13 +1,16 @@
 import express from 'express';
 import { ControllersCreateProduct } from '../../controllers/ControllersProduct/ControllersCreateProduct';
 import { ControllersGetAllProduct } from '../../controllers/ControllersProduct/ControllersGetAllProduct';
+import { ControllersGetOneUserProduct } from '../../controllers/ControllersProduct/ControllersGetOneUserProduct';
 
 const createProduct = new ControllersCreateProduct(); //criação de um produto.
 const getAllProduct = new ControllersGetAllProduct(); // buscar todos produtos cadastrados
+const getOneUserProduct = new ControllersGetOneUserProduct(); //buscar todos produtos relacionado a Admin.
 
 const RouterProduct = express.Router();
 
 RouterProduct.post('/products', createProduct.handle);
 RouterProduct.get('/products', getAllProduct.handle);
+RouterProduct.get('/products/one', getOneUserProduct.handle);
 
 export { RouterProduct };
