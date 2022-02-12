aa83bac2dbff9f41b4fac52cb0f8c040ed0f055d
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-12 00:52:22 -0300

    finalizado/controllers: foi finalizado controlores para atualizar um produto cadastrado.

---
+++ src/controllers/ControllersProduct/ControllerUpdatedProduct.ts
@@ -0,0 +1,31 @@
+import { Request, Response } from 'express';
+import { ServicesUpdatedProduct } from '../../services/ServicesProducts/ServicesUpdatedProduct';
+
+class ControllerUpdatedProduct {
+  async handle(request: Request, response: Response) {
+    const { id } = request.params;
+    const { name, description, price, quantity, user_id } = request.body;
+
+    const userService = new ServicesUpdatedProduct();
+
+    const result = await userService.execute({
+      id,
+      name,
+      description,
+      price,
+      quantity,
+      user_id,
+    });
+    if (result instanceof Error) {
+      return response.json(result.message);
+    }
+
+    return response.status(200).json({
+      status: 200,
+      messeger: 'Produto foi atualizada com sucesso!',
+      addres: result,
+    });
+  }
+}
+
+export { ControllerUpdatedProduct };
