7ea6bc760d4ae27b0028602b92913b63b84d885e
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-12 04:12:08 -0300

    finalizado/services: finalido services para criar pagamentos.

---
+++ src/services/ServicesPayment/ServiceCreatePayment.ts
@@ -0,0 +1,52 @@
+import { getCustomRepository } from 'typeorm';
+import { RepositoriesPayment } from '../../repositories/RepositoriePayment';
+
+interface IPaymentProps {
+  obeservation: string;
+  type_payment: string;
+  name_expresso_card: string;
+  number_card: number;
+  date_card: string;
+  cvv_card: number;
+  quantity: number;
+  resumo: string;
+  toti: string;
+  card_id?: string;
+  user_id: string;
+}
+
+class CreateUserService {
+  async execute({
+    obeservation,
+    type_payment,
+    name_expresso_card,
+    date_card,
+    cvv_card,
+    quantity,
+    resumo,
+    toti,
+    card_id,
+    user_id,
+  }: IPaymentProps) {
+    const paymentReposotry = getCustomRepository(RepositoriesPayment);
+
+    const payment = paymentReposotry.create({
+      obeservation,
+      type_payment,
+      name_expresso_card,
+      date_card,
+      cvv_card,
+      quantity,
+      resumo,
+      toti,
+      card_id,
+      user_id,
+    });
+
+    await paymentReposotry.save(payment);
+
+    return payment;
+  }
+}
+
+export { CreateUserService };
