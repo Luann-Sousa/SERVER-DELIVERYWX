ff3a872ccbbda791295acd586fc46c2d888286bc
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-11 23:45:28 -0300

    criação/rotas: foi criado rota para criar um produto no sistema.

--- src/routes/productRoutes/productRoutes.ts
+++ src/routes/productRoutes/productRoutes.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 import { ControllersCreateProduct } from '../../controllers/ControllersProduct/ControllersCreateProduct';
 
-const createProduct = new ControllersCreateProduct(); //crição de u produto.
+const createProduct = new ControllersCreateProduct(); //criação de um produto.
 
 const RouterProduct = express.Router();
 
