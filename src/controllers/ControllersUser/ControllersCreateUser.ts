7a941857103fb85a8f188ab42b4c65f295912b3a
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-14 01:40:28 -0300

    instalação/lib: foi instalado a lib multer para trabalharmos com upload de images com nodeJs

--- src/controllers/ControllersUser/ControllersCreateUser.ts
+++ src/controllers/ControllersUser/ControllersCreateUser.ts
@@ -3,6 +3,8 @@ import { CreateUserService } from '../../services/ServicesUser/SeviceCreateUser'
 
 class UserCreateControler {
   async handle(request: Request, response: Response) {
+    console.log('kkkkk');
+
     const {
       name,
       username,
@@ -14,7 +16,7 @@ class UserCreateControler {
       nivel_user,
       date_nasc,
     } = request.body;
-
+    console.log('req', phone);
     const createUser = new CreateUserService();
 
     const user = await createUser.execute({
