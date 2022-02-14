0f453903068e5a7fd4c686fe84075b5875926758
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-14 00:08:27 -0300

    finalizado/entities: foi finalizado entities para criar empresas.

---
+++ src/entities/Company.ts
@@ -0,0 +1,75 @@
+import {
+  Entity,
+  PrimaryColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+  ManyToOne,
+  JoinColumn,
+  ManyToMany,
+} from 'typeorm';
+import { v4 as uuidV4 } from 'uuid';
+import { User } from './User';
+import { Address } from './Address';
+
+@Entity('company')
+class Company {
+  @PrimaryColumn()
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  file: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  cnpj: string;
+
+  @Column()
+  assessment: number;
+
+  @Column()
+  delivery_feed: number;
+
+  @Column()
+  delivery_time: number;
+
+  @Column()
+  user_id: string;
+
+  @Column()
+  address_id: string;
+
+  @ManyToOne(() => User, {
+    cascade: true,
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({ name: 'address_id' }) ///qual coluna dentro da minha tabela de videos que tou referenciando
+  user: User;
+
+  @ManyToMany(() => Address, {
+    cascade: true,
+    onDelete: 'CASCADE',
+    onUpdate: 'CASCADE',
+  })
+  @JoinColumn({ name: 'user_id' }) ///qual coluna dentro da minha tabela de videos que tou referenciando
+  address: Address[];
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+    }
+  }
+}
+export { Company };
