ff8d0eab75f1629f05fbc5d79bf34a4ef0228e08
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-14 00:47:17 -0300

    criação/rota: foi criado rota para buscar empressas relacionado a endereço e usuario.

--- src/routes/companyRoutes/companyRoutes.ts
+++ src/routes/companyRoutes/companyRoutes.ts
@@ -3,13 +3,16 @@ import multer from 'multer';
 import createMulter from '../../utils/upload';
 
 import { ControllerCreateCompany } from '../../controllers/ControllersCompany/ControllersCreateCompany';
+import { ControllersGetOneRelatCompany } from '../../controllers/ControllersCompany/ControllersGetOneRelatCompany';
 
-const createCompany = new ControllerCreateCompany();
+const createCompany = new ControllerCreateCompany(); //criação de uma empresa
+const getOneRelatCompany = new ControllersGetOneRelatCompany(); //buscando todoas empresas relacionados a user/addres.
 
 const RouteCompany = express.Router();
 
-const upload = multer(createMulter);
+const upload = multer(createMulter); //instânciando o multer para já usarmos.
 
-RouteCompany.post('/v1/company', upload.single('file'), createCompany.handle); //instânciando o multer para já usarmos.
+RouteCompany.post('/v1/company', upload.single('file'), createCompany.handle);
+RouteCompany.get('/v1/company', getOneRelatCompany.handle);
 
 export { RouteCompany };
