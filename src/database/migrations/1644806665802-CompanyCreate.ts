737d16b28c569115068801f2ac9451244dfa5c57
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-14 00:01:34 -0300

    finalizado/migrations: foi finalizado migration para criar uma empresa.

--- src/database/migrations/1644806665802-CompanyCreate.ts
+++ src/database/migrations/1644806665802-CompanyCreate.ts
@@ -1,11 +1,87 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
 
 export class CompanyCreate1644806665802 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    return queryRunner.createTable(
+      new Table({
+        name: 'company',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            isGenerated: true,
+          },
+          {
+            name: 'name',
+            type: 'varchar',
+          },
+          {
+            name: 'file',
+            type: 'varchar',
+          },
+          {
+            name: 'description',
+            type: 'varchar',
+          },
+          {
+            name: 'cnpj',
+            type: 'varchar',
+          },
+          {
+            name: 'assessment',
+            type: 'numeric',
+          },
+          {
+            name: 'delivery_fee',
+            type: 'numeric',
+          },
+          {
+            name: 'delivery_time',
+            type: 'varhcar',
+          },
+          {
+            name: 'user_id',
+            type: 'uudi',
+          },
+          {
+            name: 'address_id',
+            type: 'uudi',
+          },
+          {
+            name: 'created_at',
+            type: 'timestap',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestap',
+            default: 'now()',
+          },
+        ],
+        foreignKeys: [
+          {
+            name: 'fk_company_users',
+            columnNames: ['user_id'],
+            referencedTableName: 'users',
+            referencedColumnNames: ['id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+          {
+            name: 'fk_company_address',
+            columnNames: ['address_id'],
+            referencedTableName: 'address',
+            referencedColumnNames: ['id'],
+            onDelete: 'CASCADE',
+            onUpdate: 'CASCADE',
+          },
+        ],
+      }),
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    queryRunner.dropTable('company');
+  }
 }
