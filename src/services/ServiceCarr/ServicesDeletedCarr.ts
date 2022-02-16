ddc23c8f287334e6aa33b889ce6c388b435543aa
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-16 00:34:43 -0300

    finalziado/services: foi finalizado services para excluir um carrinho do sistema.

--- src/services/ServiceCarr/ServicesDeletedCarr.ts
+++ src/services/ServiceCarr/ServicesDeletedCarr.ts
@@ -9,9 +9,9 @@ class ServicesDeletedCarr {
   async execute({ id_carr }: IProductProps) {
     const repositorieDeleted = getCustomRepository(RepositoriesCarr);
 
-    const productAllExists = await repositorieDeleted.findOne(id_carr);
+    const carrAllExists = await repositorieDeleted.findOne(id_carr);
 
-    if (!productAllExists) {
+    if (!carrAllExists) {
       return new Error('Não existe esse produto cadastrado tente outro !');
     }
     const carrtDeleted = await repositorieDeleted.delete(id_carr);
