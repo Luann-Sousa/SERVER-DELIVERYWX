353baffbe5eb5124a8b06d9b317d76ab33c0b809
weversonluan17@gmail.com <weversonluan17@gmail.com>
2022-02-15 21:39:06 -0300

    finalizado/controllers: foi finalizado o controllers para criar nosso carrinho de comprar.,

--- src/routes/carrRoutes/carrRoutes.ts
+++ src/routes/carrRoutes/carrRoutes.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { ControllersCreateCarr } from '../../controllers/ControllersCarr/ControllersCreateCarr';
+import { ControllersGetAllCarr } from '../../controllers/ControllersCarr/ControllersGetAllCarr';
 
 const createCarr = new ControllersCreateCarr(); //criação de um carrinho de compra
+const getAtAllCarr = new ControllersGetAllCarr(); //buscado todos pedico adicionado ao um carinho
 
 const RouterCarr = express.Router();
 
 RouterCarr.post('/v1/carrs', createCarr.handle);
+RouterCarr.get('/v1/carrs', getAtAllCarr.handle);
 
 export { RouterCarr };
